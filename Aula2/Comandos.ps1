dbf93957c0b487970d8e78e8bd3e603a15c966c9
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-07-21 19:48:45 -0300

    Partes 2 e 3 da aula 2 do curso PowerShell parte 1: Começando com a linguagem de scripting moderna.

--- Aula2/Comandos.ps1
+++ Aula2/Comandos.ps1
@@ -1,24 +1,103 @@
-Write-Host "CommandPrecedence:"
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Set-Location -Path $scriptPath
 
-# Saindo do diretório atual se ele começa com a string 'Aula'
-if ((Split-Path -Path((Get-Location).Path) -Leaf).StartsWith(("Aula"))) {
-    Set-Location -Path ".."
-}
-# Entrando na pasta 'Aula1'
-Set-Location -Path "./Aula1"
+function TestarCommandPrecedence {
+    Write-Output "Testando o CommandPrecedence:"
+    Write-Output ""
 
-Write-Host "Tentando executar o scrpt batch da aula passada sem o ""CommandPrecedence""..."
-try {
-    reiniciarServicoByteBankSrv01.bat
-}
-catch {
-    Write-Error $_
+    Set-Location -Path "../Aula1"
+
+    Write-Output "Tentando executar o scrpt batch da aula passada sem o ""CommandPrecedence""..."
+    try {
+        reiniciarServicoByteBankSrv01.bat
+    }
+    catch {
+        Write-Error $_
+    }
+
+    Write-Output "Tentando executar o script bash da aula passada com o ""CommandPrecedence""..."
+    try {
+        ./reiniciarServicoByteBankSrv01.bat
+    }
+    catch {
+        Write-Error $_
+    }
+
+    Write-Output "Tentando executar o ping por meio de: ""ping google.com"""
+    try {
+        ping google.com -n 10
+    }
+    catch {
+        $_
+    }
+
+    Set-Location -Path (Split-Path -Path $scriptPath)
+
+    Write-Output "----------------------------------------------------------------------------------------------------------------------"
 }
 
-Write-Host "Tentando executar o script bash da aula passada com o ""CommandPrecedence""..."
-try {
-    ./reiniciarServicoByteBankSrv01.bat
+function TestarFuncaoGetType {
+
+    Write-Output "Testando a função Get-Type"
+    Write-Output ""
+
+    Write-Output "$($env:Path.GetType())"
+    Write-Output ""
+
+    Write-Output "----------------------------------------------------------------------------------------------------------------------"
 }
-catch {
-    Write-Error $_
+
+function MostrarVariavelPath {
+
+    Write-Output "Mostrando a variável de ambiente Path"
+    Write-Output ""
+
+    Write-Output "Mostrando a variável de ambiente Path no Prompt de Comando:"
+    Write-Output ""
+    try {
+        cmd /C "echo %PATH%"
+    }
+    catch {
+        Write-Error $_
+    }
+    Write-Output ""
+
+    Write-Output "Mostrando a variável de ambiente no PowerShell:"
+    Write-Output ""
+    Write-Output $env:Path
+    Write-Output ""
+
+    Write-Output "Mostrando a variável de ambiente no PowerShell com quebra de linha:"
+    Write-Output ""
+    $variavelAmbientePath = $env:Path.Split(";")
+    foreach ($caminho in $variavelAmbientePath) {
+        Write-Output $caminho
+    }
+    Write-Output ""
+
+    Write-Output "----------------------------------------------------------------------------------------------------------------------"
 }
+
+function TestarFuncaoGetMember {
+    
+    Write-Output "Testando a função Get-Member"
+    Write-Output ""
+
+    $membros = Get-Member -InputObject $env:Path
+
+    foreach ($membro in $membros) {
+        Write-Output $membro
+    }
+    Write-Output ""
+
+    Write-Output "----------------------------------------------------------------------------------------------------------------------"
+}
+
+function  Main {
+    TestarCommandPrecedence
+    TestarFuncaoGetType
+    MostrarVariavelPath 
+    TestarFuncaoGetMember  
+}
+
+Main
